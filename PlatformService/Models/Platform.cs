c0328a9444892526dbbe5224de377c2240d676cf
lollipop <hiep@elitetech.vn>
2021-09-01 13:50:43 +0700

    first commit

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        public string Cost { get; set; }
+    }
+}
